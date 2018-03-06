34360999f2eab15555fe0f0b8af646258d41872d
David Sykes <david.sykes@octoenergy.com>
2018-03-06 00:22:35 +0000

    Add a new model class that directly pickles the transformer

--- api/utils/models.py
+++ api/utils/models.py
@@ -1,6 +1,5 @@
 from sklearn.ensemble import RandomForestRegressor
-
-from api.utils.transformer import Transformer
+from api.utils.transformers import FullTransformer
 import pickle
 
 class TWCModel(object):
@@ -12,21 +11,22 @@ class TWCModel(object):
         return RandomForestRegressor(n_jobs=-1, n_estimators=1000)
 
     def fit(self, tables):
-        self.transformer = Transformer()
+        self.transformer = FullTransformer()
         self.model = self.get_model()
         X, y = self.transformer.fit_transform(tables)
         self.model.fit(X, y)
 
     def predict(self, tables):
-        X = self.transformer.transform(tables, False)
+        X = self.transformer.transform(tables)
         return self.model.predict(X)
 
     def save(self, file_name):
         with open(file_name, 'wb') as file:
-            pickle.dump({'cols': self.transformer.column_schema, 'model': self.model}, file)
+            pickle.dump({'transformer':self.transformer,
+                        'model': self.model}, file)
 
     def load(self, file_name):
         with open(file_name, 'rb') as file:
             manifest = pickle.load(file)
-            self.transformer = Transformer(manifest['cols'])
+            self.transformer = manifest['transformer']
             self.model = manifest['model']
